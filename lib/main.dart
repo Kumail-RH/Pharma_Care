c17de7f5b752f12dacef3a6238bc9695f06e519c
Kumail-RH <kumailrazahaider@gmail.com>
2025-03-27 17:09:36 +0500

    Almost done

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:inventory_management_system/services/auth_service.dart';
 import 'package:inventory_management_system/utility/constants.dart';
 import 'package:inventory_management_system/utility/theme.dart';
 import 'package:provider/provider.dart';
-
 // void main() async {
 //   WidgetsFlutterBinding.ensureInitialized();
 //   await Firebase.initializeApp();
